a7d1ad6f2f8e319e98924ee164430d1fb7d564e1
Jasperino64 <jasperschen@gmail.com>
2023-12-14 20:51:45 -0500

    Meta tags

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+// Header.js
+import React from 'react';
+import logo from '../Logo.svg'; // replace with your logo path
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Logo" />
+    </header>
+  );
+}
+
+export default Header;
+
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+// Nav.js
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        // add more navigation links as needed
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
